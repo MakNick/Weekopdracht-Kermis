a524e873feef1f8b065f695c2077140ae9924ca6
MakNick <nick_makkinga@hotmail.com>
2018-10-23 19:48:21 +0200

    23.10.2018 doelstelling 3

--- RisicoRijkeAttractie.java
+++ RisicoRijkeAttractie.java
@@ -8,12 +8,18 @@ abstract public class RisicoRijkeAttractie extends Attractie {
 		omzetAttractie += attractiePrijs;
 		Kassa.totaleOmzet += attractiePrijs;
 		aantalRitten++;
-		opstellingsKeuring();
+		onderhoudsKeuring();
 	}
 	
 	void opstellingsKeuring() {
+		if(aantalRitten == 0) {
+			System.out.println("De kermis is opgebouwd, " + attractieNaam + " is gekeurd en klaar voor gebruik.");
+		}
+	} 
+	
+	void onderhoudsKeuring() {
 		if((aantalRitten % draaiLimiet) == 0) {
-			System.out.println("De monteur is geweest, " + attractieNaam + " is gekeurd.");
+			System.out.println("De monteur is geweest, " + attractieNaam + " is gekeurd en kan weer gebruikt worden.");
 		}
 	} 
 }
